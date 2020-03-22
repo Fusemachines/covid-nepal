4a50f536e2d3cab070de41dd19c4702d278f6268
rokinfuse <rokin@fusemachines.com>
2020-03-22 20:03:45 +0545

    Auto generate created and updated date

--- src/services/virus-count.service.ts
+++ src/services/virus-count.service.ts
@@ -4,7 +4,7 @@ import { VirusCountModel } from "../models/virus-count.model";
 export class VirusCountService {
     getVirusCountsToday() {
         try {
-            return VirusCountModel.findOne({}, {}, { sort: { 'createdDate': -1 } });
+            return VirusCountModel.findOne({}, {}, { sort: { 'createdAt': -1 } });
         } catch (error) {
             throw new Error(error);
         }
@@ -12,7 +12,7 @@ export class VirusCountService {
 
     getLatestVirusCounts() {
         try {
-            return VirusCountModel.find({}, {}, { sort: { 'createdDate': -1 } }).limit(4);
+            return VirusCountModel.find({}, {}, { sort: { 'createdAt': -1 } }).limit(4);
         } catch (error) {
             throw new Error(error);
         }
@@ -41,7 +41,7 @@ export class VirusCountService {
             let query = VirusCountModel.find({});
             const data = await query.skip(page * size).limit(size).exec();
 
-            const totalItems = await VirusCountModel.count({});
+            const totalItems = await VirusCountModel.countDocuments({});
             const totalPages = Math.ceil(totalItems / size);
 
             return {
