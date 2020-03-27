cb576266d2302153b5be40585be4c343baa9c392
fuserabin <rabin@fusemachines.com>
2020-03-27 23:36:25 +0545

    comment removed from ecosystem

--- newsfeed/ecosystem.config.js
+++ newsfeed/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'News Feed',
-    script: 'npm',
+    script: 'yarn',
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     args: 'start',
     instances: "max",
@@ -10,11 +10,11 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      DATABASE_HOST: 'localhost', // database endpoint
+      DATABASE_HOST: 'localhost',
       DATABASE_PORT: '5432',
-      DATABASE_NAME: 'strapi', // DB name
-      DATABASE_USERNAME: '', // your username for psql
-      DATABASE_PASSWORD: '', // your password for psql
+      DATABASE_NAME: 'strapi',
+      DATABASE_USERNAME: '',
+      DATABASE_PASSWORD: '',
     }
   }]
 };
