f8809d36689c03f0864ac5f58b8b7ae13729d6fc
Prashanna JBR <prashanna.rana@fusemachines.com>
2020-03-31 18:06:26 +0545

    removed db from ecosystem

--- newsfeed/ecosystem.config.js
+++ newsfeed/ecosystem.config.js
@@ -2,12 +2,6 @@ module.exports = {
   apps : [{
     name: 'newsfeed',
     script: './server.js',
-    instances: "max",
-    env: {
-      NODE_ENV: "development",
-      HOST: 'api-dev.covidnepal.org',
-      DATABASE_URI: "mongodb://newsfeeddb-usr:e7kLrKm07JWykd25sQflPs@10.0.0.10:27116/?authSource=admin",
-      DATABASE_NAME: "newsfeed"
-    }
+    instances: "max"
   }]
 };
