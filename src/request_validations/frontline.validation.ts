75167e7629d0daf8315362eaf24c9d60e4adcf7d
pratikfuse <pratik.manandhar@fusemachines.com>
2020-04-02 17:20:11 +0545

    added validations for create apis

---
+++ src/request_validations/frontline.validation.ts
@@ -0,0 +1,67 @@
+import Joi from "joi";
+import { CRequest, CResponse } from "../shared/interfaces/http.interface";
+import { NextFunction } from "express";
+
+export const validateCreateSupporter = (req: CRequest, res: CResponse, next: NextFunction) => {
+    const { body } = req;
+
+    const createSupporterSchema = {
+        name: Joi.string().required(),
+        contact: {
+            email: Joi.string().required(),
+            mobile: Joi.string(),
+            landLine: Joi.string(),
+        },
+        organization: Joi.string().required(),
+        isVerified: Joi.boolean(),
+        location: Joi.string(),
+        providedItems: Joi.array().items(Joi.string()),
+        others: Joi.string()
+    }
+
+    const result = Joi.validate(body, createSupporterSchema);
+    const { error } = result;
+
+    if (error && error.details) {
+        res.status(422).json({
+            message: 'Invalid request',
+            errors: error.details
+        })
+    } else {
+        next()
+    }
+}
+
+
+export const validateCreateRequest = (req: CRequest, res: CResponse, next: NextFunction) => {
+    const { body } = req;
+
+    const createRequestSchema = {
+        name: Joi.string().required(),
+        contact: {
+            email: Joi.string().required(),
+            mobile: Joi.string(),
+            landLine: Joi.string(),
+        },
+        organization: Joi.string().required(),
+        isVerified: Joi.boolean(),
+        isFulfilled: Joi.boolean(),
+        location: Joi.string(),
+        requestedItems: Joi.array().items(Joi.string()),
+        fulfilledBy: Joi.array().items(Joi.string()),
+        others: Joi.string()
+    }
+
+    const result = Joi.validate(body, createRequestSchema);
+    const { error } = result;
+
+    if (error && error.details) {
+        res.status(422).json({
+            message: 'Invalid request',
+            errors: error.details
+        })
+    } else {
+        next()
+    }
+}
+
