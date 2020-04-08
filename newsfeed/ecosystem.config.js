79cec60ca7a7aeee1fe5bb31e37f1d832dc63adf
rokinfuse <rokin@fusemachines.com>
2020-04-08 17:34:09 +0545

    Remove strapi components

--- newsfeed/ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apps : [{
-    name: 'newsfeed',
-    script: './server.js',
-    instances: "max"
-  }]
-};
--- newsfeed/server.js
+++
@@ -1,2 +0,0 @@
-const strapi = require('strapi');
-strapi().start();
